5d2685e39739c901336ab3d8ad5636be37b9d6c9
Artur Barseghyan <artur.barseghyan@gmail.com>
2024-05-15 17:20:58 +0200

    Initial (#1)

---
+++ Casks/app-menu@0.1.2.rb
@@ -0,0 +1,46 @@
+cask "app-menu@0.1.2" do
+  version "0.1.2"
+  sha256 "1113703cc1b2199be646a73da33a0e1d9598d127517d95988caab4bb547369b4"
+
+  url "https://github.com/barseghyanartur/app-menu/files/14272877/ApplicationMenu.zip"
+  name "App Menu"
+  desc "Description of your application"
+  homepage "https://github.com/barseghyanartur/app-menu"
+
+  container type: :naked
+
+  stage_only true
+
+  postflight do
+    zip_path = "#{staged_path}/ApplicationMenu.zip"
+    extract_dir = "#{staged_path}/extracted"
+
+    # Unzip the downloaded file
+    system_command "unzip", args: ["-d", extract_dir, zip_path]
+
+    dmg_path = "#{extract_dir}/ApplicationMenu.dmg"
+
+    if File.exist?(dmg_path)
+      # Mount the dmg file
+      system_command "hdiutil",
+                     args: ["attach", "-nobrowse", dmg_path]
+
+      # Delete any existing application to force-replace
+      if File.exist?("/Applications/ApplicationMenu.app")
+        system_command "rm", args: ["-rf", "/Applications/ApplicationMenu.app"]
+      end
+
+      # Copy the new application to /Applications
+      system_command "cp", 
+                     args: ["-r", "/Volumes/ApplicationMenu/ApplicationMenu.app", "/Applications/"]
+
+      # Unmount the dmg file
+      system_command "hdiutil",
+                     args: ["detach", "/Volumes/ApplicationMenu"]
+    else
+      raise "DMG file not found: #{dmg_path}"
+    end
+  end
+
+  uninstall delete: "/Applications/ApplicationMenu.app"
+end
---
+++ README.rst
@@ -0,0 +1,28 @@
+============
+app-menu-tap
+============
+.. References
+
+.. _homebrew cask: https://github.com/Homebrew/homebrew-cask
+.. _tap: https://docs.brew.sh/Taps
+.. _app-menu: https://github.com/barseghyanartur/app-menu
+
+A `homebrew cask`_ `tap`_ for `app-menu`_ - the missing Applications Menu 
+for macOs.
+
+Install
+=======
+.. code-block:: sh
+
+    brew tap barseghyanartur/app-menu-tap
+    brew install --cask app-menu
+
+Uninstall
+=========
+.. code-block:: sh
+
+    brew uninstall --cask app-menu
+
+License
+=======
+MIT
